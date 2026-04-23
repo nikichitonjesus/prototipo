74fbb8d5fffc690384270a02fed96c6e83d74f88
nikichitonjesus <nikichitonjesus@gmail.com>
2026-04-22 18:06:23 -0600

    Update show.js

--- show.js
+++ show.js
@@ -1,17 +1,3 @@
-// show.js - Punto de entrada para compatibilidad con main.js
-// Re-exporta todas las funciones y variables que antes estaban aquí
-
-export { DATA } from './js/components/utils.js';
-export { renderFeed } from './js/components/feed.js';
-export { renderGrid, renderSeriesGrid, showItemsGrid, showSeriesGrid } from './js/components/grid.js';
-export { renderEpisodio, renderSerie } from './js/components/detail.js';
-export { renderCategoryPills } from './js/components/categoryPills.js';
-
-// También exportamos las funciones que main.js podría necesitar (aunque no las use directamente)
-export { ICONS, CATEGORIES } from './js/components/constants.js';
-export { determineCategories, getRandomSafe, showCustomAlert } from './js/components/utils.js';
-export { createStandardCard, createVerticalCard, createVideoExpand, createListItem, createGridCard } from './js/components/cards.js';
-export { createCarousel, createSeriesCarousel, createRecommendedCarousel } from './js/components/carousel.js';
-
-// Las funciones globales ya se asignaron a window en grid.js, pero por si acaso se exportan
-export { shareContent, handlePlay, handleDl, handleAdd, goToDetail, handleCategoryClick } from './js/components/grid.js';
+// show.js - Punto de entrada principal (barrel)
+// Mantiene compatibilidad con main.js y el resto del código
+export * from './show/index.js';
